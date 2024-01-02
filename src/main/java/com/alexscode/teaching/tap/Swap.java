045d1c3267f21e42b8150cf1d88e5df638f3baa4
Axelb2b <brunelaxel@gmail.com>
2024-01-02 16:57:14 +0100

    Algo Glouton final
    Algo terminé glouton

--- src/main/java/com/alexscode/teaching/tap/Swap.java
+++ src/main/java/com/alexscode/teaching/tap/Swap.java
@@ -1,38 +1,39 @@
 package com.alexscode.teaching.tap;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import com.alexscode.teaching.utilities.Element;
 
-public class AlgoGlouton implements TAPSolver{
-
+public class Swap implements TAPSolver{
     @Override
     public List<Integer> solve(Instance ist) {
-        Objectives obj = new Objectives(ist);
-        //Création de la solution (vide), de la listes de ratios (vide) 
-        List<Integer> demo = new ArrayList<>();
-        List<Element> ratios = new ArrayList<>();
         
-        //création de la lite de ratios coût/interêt pour chaque objet (condition de l'algo de liste)
+        List<Integer> demo = new ArrayList<>();
+        Objectives obj = new Objectives(ist);
+        List<Element> ratios = new ArrayList<>();
+        List<Element> voisins = new ArrayList<>();
+        
+        
         for (int i = 0; i< ist.size; i++){
             ratios.add(new Element(i,ist.costs[i]/ist.interest[i]));
+
         }
 
-        //Tri de la liste d'Element sur la valeur
         Collections.sort(ratios);
-        //compteur de sortie pour parcourir
-        int index1 = 0;
+        
+         int index1 = 0;
 
-        //boucle qui va remplir la solution  jusqu'à la limite (coût ou distance)
         while (obj.distance(demo) < ist.getMaxDistance() && obj.time(demo) < ist.getTimeBudget()){
-            demo.add(ratios.get(index1++).index);    
+           
+            demo.add(ratios.get(index1++).index);
+            
         }
-        //Enlever la requête qui fait "déborder" là solution
         demo.subList(0, demo.size() - 1);
+    
+    // Ajout de la recherche locale avec l'opérateur SWAP
+    double min_sol = demo.stream().mapToDouble(j -> ist.interest[j]).sum();
     
 
 return demo; 
@@ -40,3 +41,4 @@ return demo;
     
 }
 }
+    
