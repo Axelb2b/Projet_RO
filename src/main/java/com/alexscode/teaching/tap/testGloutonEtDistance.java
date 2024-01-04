d3dfe938da2b1b77a2c550959e35341dadc415e7
ManonLacombe <115247796+ManonLacombe@users.noreply.github.com>
2024-01-04 16:42:59 +0100

    2ème partie testGloutonEtDistance

--- src/main/java/com/alexscode/teaching/tap/testGloutonEtDistance.java
+++ src/main/java/com/alexscode/teaching/tap/testGloutonEtDistance.java
@@ -24,39 +24,40 @@ public class testGloutonEtDistance implements TAPSolver{
         int index1 = 0;
         List<Element> RatioNonPermanent = new ArrayList<>();
         List<Element> distancesAvecI = new ArrayList<>();
+        List<Element> NewRatioPourI = new ArrayList<>();
+        List<Element> NewRatioPourITri = new ArrayList<>();
+        List<Element> ListeBoucle = new ArrayList<>();
 
         
         while (obj.distance(demo) < ist.getMaxDistance() && obj.time(demo) < ist.getTimeBudget()){
-           //calcule la distance entre les 2 éléments (éléments de la liste de ratios)
-           /* for (int i =0; i<ratios.size()-1;i++){
-                ratioFinal=ratios;
-                int identifiant1=ratios.get(i).index;
-
-                for (int j=1;i<ratios.size();j++){
-                    int identifiant2=ratios.get(j).index;
-                    double distancesEntre1Et2 = ist.distances[identifiant1][identifiant2];
-                
-                double newRatio = ratios.get(i).value + ratios.get(j).value + distancesEntre1Et2;
-
-                ratioFinal.add(newRatio);
-                }
-                
-            }
-
-*/
             RatioNonPermanent=ratios;
+            ListeBoucle.add(ratios.get(0));
             for (int i =0; i<RatioNonPermanent.size()-1;i++){
                 int identifiant1=ratios.get(i).index;
                 for (int j=1;i<ratios.size();j++){
                     int identifiant2=ratios.get(j).index;
                     double distancesEntre1Et2 = ist.distances[identifiant1][identifiant2];
-                    distancesAvecI.add(new Element(i,distancesEntre1Et2));
+                    //distancesAvecI.add(new Element(i,distancesEntre1Et2));
                     double newRatio = ratios.get(j).value + distancesEntre1Et2;
+                    NewRatioPourI.add(new Element(i,newRatio));
                 }
+                NewRatioPourITri=NewRatioPourI;
+                Collections.sort(NewRatioPourITri);
+                int meilleur=NewRatioPourITri.get(0).index;
+                //chercher élément qui est en "meilleur" position dans la liste NewRatioPourI
+                //faire en sorte que i devienne maintenant meilleur
+                ListeBoucle.add(NewRatioPourITri.get(meilleur));
+
+                //supprimer "i" de la liste RatioNonPermanent
+                RatioNonPermanent.remove(ratios.get(i));
+
+                //vider les listes NewRatioPourI et NewRatioPourITri
+                NewRatioPourI.clear();
+                NewRatioPourITri.clear();
             }    
 
            //mettre cette ligne
-            demo.add(ratiosFinal.get(index1++).index);
+            demo.add(ListeBoucle.get(index1++).index);
             
         }
         List<Integer> sub = new ArrayList<>(demo.subList(0, demo.size()-1));
